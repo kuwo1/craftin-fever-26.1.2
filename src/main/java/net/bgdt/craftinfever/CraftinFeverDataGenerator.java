f3852111f9d631529d7263bee400c9c2606f79ff
kuwo1 <rvkeely2.0@gmail.com>
2026-06-30 15:21:35 -0500

    Initial commit

---
+++ src/main/java/net/bgdt/craftinfever/CraftinFeverDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bgdt.craftinfever;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CraftinFeverDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

0c9691a9077e1dd68f63fb8a75d03cb2bbaa24cc
kuwo1 <rvkeely2.0@gmail.com>
2026-07-01 15:48:14 -0500

    tree setup

--- src/main/java/net/bgdt/craftinfever/FeverConfiguredFeatures.java
+++ src/main/java/net/bgdt/craftinfever/FeverConfiguredFeatures.java
@@ -2,7 +2,7 @@ package net.bgdt.craftinfever;
 
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-
+//what am i even doing dog
 public class FeverConfiguredFeatures {
     public static void configure(BootstrapContext<ConfiguredFeature<?,?>> context){
 

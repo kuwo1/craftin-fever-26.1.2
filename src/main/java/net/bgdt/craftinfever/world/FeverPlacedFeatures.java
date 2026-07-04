74ca0f0e9e5a509e412a15764dd466819ccae17c
kuwo1 <rvkeely2.0@gmail.com>
2026-07-04 18:13:17 -0500

    jar commit

--- src/main/java/net/bgdt/craftinfever/world/FeverPlacedFeatures.java
+++ src/main/java/net/bgdt/craftinfever/world/FeverPlacedFeatures.java
@@ -1,4 +1,27 @@
 package net.bgdt.craftinfever.world;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
+import java.util.List;
+
+import static net.bgdt.craftinfever.world.FeverConfiguredFeatures.registerKey;
+
 public class FeverPlacedFeatures {
+  //  public static final ResourceKey<PlacedFeature> AMERICAN_BEECH_PLACED = registerKey("american_beech_placed");
+
+  //  public static void bootstrap(BootstrapContext<PlacedFeature> context){
+      //  var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+      //  var treeConfigured = configuredFeatures.getOrThrow(FeverConfiguredFeatures.AMERICAN_BEECH_TREE);
+
+     //   context.register(AMERICAN_BEECH_PLACED, new PlacedFeature(treeConfigured,
+       //         List.of(
+      //                  PlacementUtils.countExtra(10,0.1f, 1),
+      //                  InSquarePlacement.get
+       //         )));
+  //  }
 }

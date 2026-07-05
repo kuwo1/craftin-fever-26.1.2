3fe8248b75d13fba1223b3c3406f7ccceabdcc34
kuwo1 <rvkeely2.0@gmail.com>
2026-07-04 21:49:57 -0500

    what does registerKey even mean dog

--- src/main/java/net/bgdt/craftinfever/world/FeverPlacedFeatures.java
+++ src/main/java/net/bgdt/craftinfever/world/FeverPlacedFeatures.java
@@ -7,6 +7,9 @@ import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.state.predicate.BlockPredicate;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
@@ -17,16 +20,16 @@ import static net.bgdt.craftinfever.world.FeverConfiguredFeatures.registerKey;
 public class FeverPlacedFeatures {
     public static final ResourceKey<PlacedFeature> AMERICAN_BEECH_PLACED = registerKey("american_beech_placed");
 
-    public static void bootstrap(BootstrapContext<PlacedFeature> context){
+    public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
         var treeConfigured = configuredFeatures.getOrThrow(FeverConfiguredFeatures.AMERICAN_BEECH_TREE);
 
         context.register(AMERICAN_BEECH_PLACED, new PlacedFeature(treeConfigured,
                 List.of(
-                        PlacementUtils.countExtra(10,0.1f, 1),
-                       InSquarePlacementModifier.getInstance(),
-                        HeightmapPlacementModifier.of(Heightmap.Types.MOTION_BLOCKING),
-                        BlockFilterPlacementModifier.of(BlockPredicate.wouldSurvive(FeverBlocks.))
-                )));
+                        PlacementUtils.countExtra(10, 0.1f, 1),
+                        InSquarePlacement.spread(),
+                        HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6),
+                                VerticalAnchor.absolute(30)), BiomeFilter.biome())));
     }
+
 }

97c498ad867acabc8803a3fe37216d79b3a30a40
kuwo1 <rvkeely2.0@gmail.com>
2026-07-04 22:12:47 -0500

    it isn't broken now but also it doesn't work

--- src/main/java/net/bgdt/craftinfever/world/FeverPlacedFeatures.java
+++ src/main/java/net/bgdt/craftinfever/world/FeverPlacedFeatures.java
@@ -1,9 +1,11 @@
 package net.bgdt.craftinfever.world;
 
+import net.bgdt.craftinfever.CraftinFever;
 import net.bgdt.craftinfever.block.FeverBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.state.predicate.BlockPredicate;
 import net.minecraft.world.level.levelgen.Heightmap;
@@ -16,9 +18,10 @@ import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import java.util.List;
 
 import static net.bgdt.craftinfever.world.FeverConfiguredFeatures.registerKey;
+import static net.minecraft.data.worldgen.placement.PlacementUtils.createKey;
 
 public class FeverPlacedFeatures {
-    public static final ResourceKey<PlacedFeature> AMERICAN_BEECH_PLACED = registerKey("american_beech_placed");
+    public static final ResourceKey<PlacedFeature> AMERICAN_BEECH_PLACED = createKey("american_beech_placed");
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
@@ -30,6 +33,10 @@ public class FeverPlacedFeatures {
                         InSquarePlacement.spread(),
                         HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6),
                                 VerticalAnchor.absolute(30)), BiomeFilter.biome())));
+    }
+
+    public static ResourceKey<PlacedFeature> createKey(final String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, Identifier.fromNamespaceAndPath(CraftinFever.MOD_ID ,name));
     }
 
 }

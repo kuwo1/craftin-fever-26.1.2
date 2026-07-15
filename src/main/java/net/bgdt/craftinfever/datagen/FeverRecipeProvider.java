69a772e48122a3c638b2d8fed1698c40232c79fb
kuwo1 <rvkeely2.0@gmail.com>
2026-07-14 19:15:22 -0500

    fixed wool recipes for 26.2

--- src/main/java/net/bgdt/craftinfever/datagen/FeverRecipeProvider.java
+++ src/main/java/net/bgdt/craftinfever/datagen/FeverRecipeProvider.java
@@ -8,7 +8,10 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.references.BlockIds;
+import net.minecraft.references.BlockItemIds;
 import net.minecraft.references.ItemIds;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
@@ -31,13 +34,13 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                 List<ItemLike> STRAWBERRY_SMELTABLES = List.of(FeverItems.OMELETTE, FeverBlocks.WHITE_QUILT_BLOCK);
 
 
-                shaped(RecipeCategory.MISC, FeverBlocks.WHITE_QUILT_BLOCK,4)
+                shaped(RecipeCategory.MISC, FeverBlocks.WHITE_QUILT_BLOCK, 4)
                         .pattern(" A ")
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.WOOL)
-                        .unlockedBy(getHasName(ItemIds.), has(Blocks.WHITE_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.WHITE))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.WHITE)), has(Blocks.WOOL.pick(DyeColor.WHITE)))
                         .group("wool")
                         .save(output, "white_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.BLACK_QUILT_BLOCK,4)
@@ -45,8 +48,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.BLACK_WOOL)
-                        .unlockedBy(getHasName(Blocks.BLACK_WOOL), has(Blocks.BLACK_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.BLACK))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.BLACK)), has(Blocks.WOOL.pick(DyeColor.BLACK)))
                         .group("wool")
                         .save(output, "black_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.LIGHT_GRAY_QUILT_BLOCK,4)
@@ -54,8 +57,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.LIGHT_GRAY_WOOL)
-                        .unlockedBy(getHasName(Blocks.LIGHT_GRAY_WOOL), has(Blocks.LIGHT_GRAY_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.LIGHT_GRAY))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.LIGHT_GRAY)), has(Blocks.WOOL.pick(DyeColor.LIGHT_GRAY)))
                         .group("wool")
                         .save(output, "light_gray_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.GRAY_QUILT_BLOCK,4)
@@ -63,8 +66,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.GRAY_WOOL)
-                        .unlockedBy(getHasName(Blocks.GRAY_WOOL), has(Blocks.GRAY_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.GRAY))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.GRAY)), has(Blocks.WOOL.pick(DyeColor.GRAY)))
                         .group("wool")
                         .save(output, "gray_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.BLUE_QUILT_BLOCK,4)
@@ -72,8 +75,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.BLUE_WOOL)
-                        .unlockedBy(getHasName(Blocks.BLUE_WOOL), has(Blocks.BLUE_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.BLUE))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.BLUE)), has(Blocks.WOOL.pick(DyeColor.BLUE)))
                         .group("wool")
                         .save(output, "blue_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.LIGHT_BLUE_QUILT_BLOCK,4)
@@ -81,8 +84,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.LIGHT_BLUE_WOOL)
-                        .unlockedBy(getHasName(Blocks.LIGHT_BLUE_WOOL), has(Blocks.LIGHT_BLUE_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.LIGHT_BLUE))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.LIGHT_BLUE)), has(Blocks.WOOL.pick(DyeColor.LIGHT_BLUE)))
                         .group("wool")
                         .save(output, "light_blue_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.PURPLE_QUILT_BLOCK,4)
@@ -90,8 +93,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.PURPLE_WOOL)
-                        .unlockedBy(getHasName(Blocks.PURPLE_WOOL), has(Blocks.PURPLE_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.PURPLE))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.PURPLE)), has(Blocks.WOOL.pick(DyeColor.PURPLE)))
                         .group("wool")
                         .save(output, "purple_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.PINK_QUILT_BLOCK,4)
@@ -99,8 +102,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.PINK_WOOL)
-                        .unlockedBy(getHasName(Blocks.PINK_WOOL), has(Blocks.PINK_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.PINK))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.PINK)), has(Blocks.WOOL.pick(DyeColor.PINK)))
                         .group("wool")
                         .save(output, "pink_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.BROWN_QUILT_BLOCK,4)
@@ -108,8 +111,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.BROWN_WOOL)
-                        .unlockedBy(getHasName(Blocks.BROWN_WOOL), has(Blocks.BROWN_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.BROWN))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.BROWN)), has(Blocks.WOOL.pick(DyeColor.BROWN)))
                         .group("wool")
                         .save(output, "brown_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.YELLOW_QUILT_BLOCK,4)
@@ -117,8 +120,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.YELLOW_WOOL)
-                        .unlockedBy(getHasName(Blocks.YELLOW_WOOL), has(Blocks.YELLOW_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.YELLOW))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.YELLOW)), has(Blocks.WOOL.pick(DyeColor.YELLOW)))
                         .group("wool")
                         .save(output, "yellow_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.RED_QUILT_BLOCK,4)
@@ -126,8 +129,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.RED_WOOL)
-                        .unlockedBy(getHasName(Blocks.RED_WOOL), has(Blocks.RED_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.RED))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.RED)), has(Blocks.WOOL.pick(DyeColor.RED)))
                         .group("wool")
                         .save(output, "red_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.ORANGE_QUILT_BLOCK,4)
@@ -135,8 +138,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.ORANGE_WOOL)
-                        .unlockedBy(getHasName(Blocks.ORANGE_WOOL), has(Blocks.ORANGE_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.ORANGE))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.ORANGE)), has(Blocks.WOOL.pick(DyeColor.ORANGE)))
                         .group("wool")
                         .save(output, "orange_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.GREEN_QUILT_BLOCK,4)
@@ -144,8 +147,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.GREEN_WOOL)
-                        .unlockedBy(getHasName(Blocks.GREEN_WOOL), has(Blocks.GREEN_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.GREEN))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.GREEN)), has(Blocks.WOOL.pick(DyeColor.GREEN)))
                         .group("wool")
                         .save(output, "green_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.LIME_QUILT_BLOCK,4)
@@ -153,8 +156,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.LIME_WOOL)
-                        .unlockedBy(getHasName(Blocks.LIME_WOOL), has(Blocks.LIME_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.LIME))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.LIME)), has(Blocks.WOOL.pick(DyeColor.LIME)))
                         .group("wool")
                         .save(output, "lime_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.CYAN_QUILT_BLOCK,4)
@@ -162,8 +165,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.CYAN_WOOL)
-                        .unlockedBy(getHasName(Blocks.CYAN_WOOL), has(Blocks.CYAN_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.CYAN))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.CYAN)), has(Blocks.WOOL.pick(DyeColor.CYAN)))
                         .group("wool")
                         .save(output, "cyan_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.MAGENTA_QUILT_BLOCK,4)
@@ -171,8 +174,8 @@ public class FeverRecipeProvider extends FabricRecipeProvider {
                         .pattern("AFA")
                         .pattern(" A ")
                         .define('A', FeverItems.FELT)
-                        .define('F', Blocks.MAGENTA_WOOL)
-                        .unlockedBy(getHasName(Blocks.MAGENTA_WOOL), has(Blocks.MAGENTA_WOOL))
+                        .define('F', Blocks.WOOL.pick(DyeColor.MAGENTA))
+                        .unlockedBy(getHasName(Blocks.WOOL.pick(DyeColor.MAGENTA)), has(Blocks.WOOL.pick(DyeColor.MAGENTA)))
                         .group("wool")
                         .save(output, "magenta_quilt_block");
                 shaped(RecipeCategory.MISC, FeverBlocks.AMERICAN_BEECH_PLANKS,4)
